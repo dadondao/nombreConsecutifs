15ba19734609ed008e1c341892c941887e8e9460
dadondao <endaodado@gmail.com>
2022-01-01 17:28:31 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tableTest';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  tab = [];
+  isConsecutive = false;
+  message: String;
+
+
+  ngOnInit(){
+    this.form = new FormGroup({
+      number : new FormControl( null , {
+        validators: [Validators.required]
+      })
+    })
+
+  }
+
+  check_is_consecutive(){
+    for(var i = 1 ; i < this.tab.length ; i++){
+      if (this.tab[i] - this.tab[i-1] ==1){
+        this.isConsecutive = true
+      }else{
+        this.isConsecutive = false
+      }
+    }
+    if(this.isConsecutive){
+      this.message = "Ses éléments sont tous consécutifs."
+    }else{
+      this.message = "Ses éléments ne sont pas consécutifs"
+    }
+
+    }
+
+  add(){
+    this.tab.push(this.form.value.number)
+    this.form.reset();
+  }
+  done(){
+    this.check_is_consecutive();
+  }
+
 }
